9ae1f5cbfa698015f5e7e474adfaaa02d00458f9
Salvador Gascon <salvadorgascon@protonmail.com>
2023-04-07 20:45:32 +0200

    use default layout on home

--- app/controllers/web/home_controller.rb
+++ app/controllers/web/home_controller.rb
@@ -1,4 +1,7 @@
-class Web::HomeController < ApplicationController
-  def index
+module Web  
+  class HomeController < ApplicationController
+    def index
+      render(layout: 'default')
+    end
   end
 end
