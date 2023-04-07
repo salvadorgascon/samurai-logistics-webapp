8f067495a89f445ef812b8616d2beaba4d75be73
Salvador Gascon <salvadorgascon@protonmail.com>
2023-04-07 22:46:54 +0200

    Eslint fixes

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,8 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import HomeController from "./home_controller"
-application.register("home", HomeController)
+import HomeController from "./home_controller";
+
+application.register("home", HomeController);
