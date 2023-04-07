3eb15cbbf9e34c06d6024aabb30e1bb51d3fdb66
Salvador Gascon <salvadorgascon@protonmail.com>
2023-04-07 07:24:07 +0200

    Execute migrate first time

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Don't work using initializer for testing database
+  config.active_record.schema_format = :sql
 end
