26ddeedc003bddbd33ca948a0d53876c241003e9
Salvador Gascon <salvadorgascon@protonmail.com>
2023-04-07 23:49:25 +0200

    Create top anb bottom react components

--- app/javascript/core/components/app.tsx
+++ app/javascript/core/components/app.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
+import Top from "./top";
 import Header from "./header";
 import Footer from "./footer";
+import Bottom from "./bottom";
 
 export default function App() {
   return (
     <div>
+      <Top/>
       <Header />
       <Footer />
+      <Bottom/>
     </div>
   );
 }
