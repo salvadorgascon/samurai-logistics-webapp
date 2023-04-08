82bc377e033bae822b0272bcf345bf29f6154e29
Salvador Gascon <salvadorgascon@protonmail.com>
2023-04-08 22:05:52 +0200

    Add menu icon to header

--- app/javascript/components/header.tsx
+++ app/javascript/components/header.tsx
@@ -8,12 +8,23 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 
 import Button from '@mui/material/Button';
-
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Header() {
   return (<header>
     <AppBar position="static">
-    <Toolbar>                   
+    <Toolbar>               
+    <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+
       <Button  variant="text" sx={{ color: '#fff' }} component={Link} to={`/`}>Home</Button>
       <Button  variant="text"  sx={{ color: '#fff' }} component={Link} to={`products`}>Products</Button>            
     </Toolbar>
