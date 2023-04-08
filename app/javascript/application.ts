7287813eb463e1d162e9a1ce9d4fe3069d590898
Salvador Gascon <salvadorgascon@protonmail.com>
2023-04-08 14:17:09 +0200

    Add comment about load font

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -1,6 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 
+/* Load fonts (required by Material UI) */
 import '@fontsource/roboto';
 
 // Load controllers
