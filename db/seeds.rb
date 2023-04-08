f9eacdee9565d216dca77a6d2809bc018e572040
Salvador Gascon <salvadorgascon@protonmail.com>
2023-04-08 03:08:44 +0200

    Delete storage tables on seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+if Rails.env.development?
+    # HACK: Required to foce load env vars
+    require 'dotenv/load'
+end
+
+Rails.logger.debug 'Deleting data ...'
+puts 'Deleting data ...'.red
+
+ActiveRecord::Base.connection.execute('DELETE FROM active_storage_variant_records')
+ActiveRecord::Base.connection.execute('DELETE FROM active_storage_attachments')
+ActiveRecord::Base.connection.execute('DELETE FROM active_storage_blobs')
