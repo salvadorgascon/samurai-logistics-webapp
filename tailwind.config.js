499f71f75bb073142a61d9124c27516b575888dd
Salvador Gascon <salvadorgascon@protonmail.com>
2023-04-08 05:34:28 +0200

    Update tailwind config with ts and jsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './app/javascript/**/*.ts',
+    './app/javascript/**/*.jsx',
+    './app/javascript/**/*.tsx'
   ]
 }
