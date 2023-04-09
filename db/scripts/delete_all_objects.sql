2238476085db410afc88c281c5634579f3f6b2af
Salvador Gascon <salvadorgascon@protonmail.com>
2023-04-09 18:07:37 +0200

    Add script to delete all tables

---
+++ db/scripts/delete_all_objects.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS taxes;
+DROP TABLE IF EXISTS currencies;
+
+DROP TABLE IF EXISTS countries;
+DROP TABLE IF EXISTS languages;
+
+DROP TABLE IF EXISTS active_storage_attachments;
+DROP TABLE IF EXISTS active_storage_variant_records;
+DROP TABLE IF EXISTS active_storage_blobs;
+
+DROP TABLE IF EXISTS ar_internal_metadata;
+DROP TABLE IF EXISTS schema_migrations;
+
