9221697692797c2ecaff10523a0d9247780623db
Salvador Gascon <salvadorgascon@protonmail.com>
2023-04-09 21:30:28 +0200

    Remove fonts from typescript file

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -1,11 +1,5 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 
-/* Load fonts (required by Material UI) */
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 // Load controllers
 import "./controllers";
