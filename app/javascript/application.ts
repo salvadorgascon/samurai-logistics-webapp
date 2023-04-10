fb932477715652d42937e6eb32ceeeb466398f97
Salvador Gascon <salvadorgascon@protonmail.com>
2023-04-10 16:41:19 +0200

    Use roboto font from google fonts

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -1,5 +1,10 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
+
 // Load controllers
 import "./controllers";
