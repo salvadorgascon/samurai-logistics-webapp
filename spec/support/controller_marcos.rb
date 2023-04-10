4e86b8fbfe62618945855a7a66b334a397a5272c
Salvador Gascon <salvadorgascon@protonmail.com>
2023-04-10 15:18:47 +0200

    Improve controller macros for devise

--- spec/support/controller_marcos.rb
+++ spec/support/controller_marcos.rb
@@ -1,10 +1,10 @@
 module ControllerMacros
   def connect_user
     before do
-      @current_user = create(:user)
-
       @request.env['devise.mapping'] = Devise.mappings[:user]
-      sign_in @current_user
+
+      @current_user = create(:user)
+      sign_in(@current_user)
 
       # Set session variables similar on application controller
     end
